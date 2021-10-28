4acccc6f005274e43d435444f390966623427572
Cameron <camerondonner63@gmail.com>
2021-10-27 19:48:19 -0500

    Final cleanup and push

--- calculator.js
+++ calculator.js
@@ -59,35 +59,25 @@ function operate (operator, a, b) {
 Function: displayStuff
 Goals		: Displays input on calculator's display.
 */
-
 function displayStuff () {
 	if (display.textContent === "0" && this.classList.contains("operand")) {
 		display.textContent = this.textContent;
 		previousButton = this;
-		//console.log(previousButton);
 	}
 	else if (display.textContent.length < 9 && this.classList.contains("operand") && !previousButton.classList.contains("operator")) {
 		display.textContent += this.textContent;
 		previousButton = this;
-		//console.log(previousButton);
 	}
 	else if (previousButton.classList.contains("operator") && this.classList.contains("operand")) {
 		display.textContent = this.textContent;
 		previousButton = this;
-		//console.log(previousButton);
 	}
-	/*else if (previousButton.id("decimal") && this.classList.contains("operand")) {
-		display.textContent += this.textContent;
-		previousButton = this;
-	}
-	else if (previousButton.id.contains("enter") && this.classList.contains("operand")) {
-		previousOperator = "";
-		previousData = 0;
-		display.textContent = this.textContent;
-		//console.log(previousButton);
-	}*/
 }
 
+/*
+Function:	displayDecimalPoint
+Goals:		Display decimal point only if one doesn't already exist.
+*/
 function displayDecimalPoint () {
 	if (!display.textContent.includes(".") && !previousButton.classList.contains("operator")) {
 		display.textContent += ".";
@@ -99,12 +89,20 @@ function displayDecimalPoint () {
 	}
 }
 
+/*
+Function: deleteStuff
+Goals		:	Delete character at the end of the display.
+*/
 function deleteStuff () {
 	let text = display.textContent;
 	let editedText = text.slice(0, -1)
 	display.textContent = editedText;
 }
 
+/*
+Function:	changeSign
+Goals		: Change negative to positive and vice versa.
+*/
 function changeSign () {
 	let currentSign = parseFloat(display.textContent);
 	if (currentSign > 0 && display.textContent.length < 9) {
@@ -117,6 +115,10 @@ function changeSign () {
 	}
 }
 
+/*
+Function: clearDisplay
+Goals		: Entirely zero out the display and calculator of previous data.
+*/
 function clearDisplay () {
 	display.textContent = "0";
 	previousOperator = "";
@@ -124,31 +126,30 @@ function clearDisplay () {
 	currentData = 0;
 }
 
-const clear = document.querySelector("#clear");
-clear.addEventListener("click", clearDisplay);
-
-const buttons = document.querySelectorAll("button");
-buttons.forEach(button => button.addEventListener("click", displayStuff));
-
-const decimalPoint = document.querySelector("#decimal");
-decimalPoint.addEventListener("click", displayDecimalPoint);
-
-/*const operands = document.querySelectorAll(".operand");
-operands.forEach(operand => operand.addEventListener("click", displayStuff));*/
-
-const display = document.querySelector("#display");
-display.textContent = "0";
-
-const operators = document.querySelectorAll(".operator");
-const percentCalc = document.querySelector("#percent");
-const inverter = document.querySelector("#invert");
-inverter.addEventListener("click", changeSign);
-
 let previousOperator = "";
 let previousData = 0;
 let previousButton;
 let currentData = 0;
 
+const display = document.querySelector("#display");
+display.textContent = "0";
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => button.addEventListener("click", displayStuff));
+
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", clearDisplay);
+
+const decimalPoint = document.querySelector("#decimal");
+decimalPoint.addEventListener("click", displayDecimalPoint);
+
+const backspace = document.querySelector("#backspace");
+backspace.addEventListener("click", deleteStuff);
+
+const inverter = document.querySelector("#invert");
+inverter.addEventListener("click", changeSign);
+
+const percentCalc = document.querySelector("#percent");
 percentCalc.addEventListener("click", () => {
 	let regex = /[^\w\s\-]/;
 	let result = parseFloat(display.textContent)/100;
@@ -169,18 +170,17 @@ percentCalc.addEventListener("click", () => {
 	}
 })
 
-// When an operator button is pressed
+const operators = document.querySelectorAll(".operator");
 operators.forEach(operator => operator.addEventListener("click", () => {
-	// The currrent number in the display is stored as 
 	currentData = parseFloat(display.textContent);
 	let regex = /[^\w\s\-]/;
 	let result = operate(previousOperator, previousData, currentData);
 	let resultString = result.toString();
+	
 	previousButton = operator;
 	previousData = result;
 	previousOperator = operator.textContent;
-	console.log(typeof(result));
-	console.log(resultString.search(regex));
+
 	if (result > 0 && resultString.search(regex) > 0 && resultString.search(regex) < 8) {
 		result = result.toFixed(8-resultString.search(regex));
 		result = +result;
@@ -207,17 +207,3 @@ operators.forEach(operator => operator.addEventListener("click", () => {
 		display.textContent = result.toExponential(2);
 	}
 }))
-
-const backspace = document.querySelector("#backspace");
-backspace.addEventListener("click", deleteStuff);
-
-/*
-When a button is clicked, do two things: update the display and store it as data.
-Update the display: Just take the button that was pressed and display its text. Some exceptions exist, worry about them later.
-Store the data:
-Receive button input.
-Push number data into an array of max length 9.
-If an operator is pressed, join the array.
-Parse the joined array to a number.
-
-*/
